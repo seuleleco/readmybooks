d05ece6ced2cf29f2973dca09c8729d446e2d4db
Leonardo Pereira <leonardo.pereira1650@outlook.com>
2025-07-27 15:15:33 -0300

    working

--- script.js
+++ script.js
@@ -1,44 +1,57 @@
 const playBtn = document.getElementById("playbtn");
 const stopBtn = document.getElementById("stopbtn");
 const continuaBtn = document.getElementById("resume");
+const cancelBtn = document.getElementById("cancel");
 const textArea = document.getElementById("input");
+const msg = document.getElementById("msg");
 
+//ATRIBUIÇÃO DE FUNÇÕES AO DISPLAY
 playBtn.addEventListener("click", iniciar);
-stopBtn.addEventListener("click", parar);
+stopBtn.addEventListener("click", pausar);
+continuaBtn.addEventListener("click", continuar);
+cancelBtn.addEventListener("click", parar);
 
-const synth = window.speechSynthesis;
+//FUNÇÕES DOS BOTOES DO DISPLAY
+const synthesizer = window.speechSynthesis;
 
-synth.addEventListener("voiceschanged", () => {
-  const voices = synth.getVoices();
-  for (const voice of voices) {
-    const option = document.createElement("option");
-    option.textContent = `${voice.name} (${voice.lang})`;
-
-    /*if (voice.default) {
-      option.textContent += " - DEFAULT";
-    }*/
-    option.setAttribute("data-lang", voice.lang);
-    option.setAttribute("data-name", voice.name);
-    /*document.getElementById("voiceSelect").*/
-    voiceSelect.appendChild(option);
-  }
-});
-/*
-populateVoiceList();
-if (
-  typeof speechSynthesis !== "undefined" &&
-  speechSynthesis.onvoiceschanged !== undefined
-) {
-  speechSynthesis.onvoiceschanged = populateVoiceList;
-}
-*/
-function iniciar() {
-  const textContent = textArea.value;
-  const synthesizer = window.speechSynthesis;
-  let voice = new SpeechSynthesisUtterance(textContent);
-  synthesizer.speak(voice);
-}
+//ESSA FUNÇÃO SERVE PARA PARAR O AUDIO QUANDO INICIAR A PAGINA, RESETANDO A FILA
+window.onload = function () {
+  synthesizer.cancel();
+};
 
 function parar() {
-  const synthesizer = window.speechSynthesis;
+  if (confirm("Deseja realmente parar a fala?")) {
+    synthesizer.cancel();
+  }
+  return;
 }
+function iniciar() {
+  //CONDIÇÃO PARA CASO A FALA ESTEJA EM ANDAMENTO
+  if (synthesizer.speaking) {
+    msg.textContent = "fala em andamento";
+    msg.classList.add("visivel");
+    //TEMPO DE VISIBILIDADE DA MENSAGEM
+    setTimeout(() => {
+      msg.classList.remove("visivel");
+    }, 1400);
+  } else {
+    // synthesizer.cancel();
+    const textContent = textArea.value;
+    let voice = new SpeechSynthesisUtterance(textContent);
+    synthesizer.speak(voice);
+  }
+}
+
+function pausar() {
+  const pausado = synthesizer.paused;
+  if (synthesizer.speak) {
+    pausado === false;
+  }
+  synthesizer.pause();
+}
+
+function continuar() {
+  synthesizer.resume();
+}
+
+console.log(synthesizer);
