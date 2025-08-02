510afca0be618e66ef82d9a93c44befeabfa6219
Leonardo Pereira <leonardo.pereira1650@outlook.com>
2025-08-02 15:25:00 -0300

    beta test

--- script.js
+++ script.js
@@ -1,57 +1,68 @@
-const playBtn = document.getElementById("playbtn");
-const stopBtn = document.getElementById("stopbtn");
-const continuaBtn = document.getElementById("resume");
-const cancelBtn = document.getElementById("cancel");
-const textArea = document.getElementById("input");
-const msg = document.getElementById("msg");
+window.speechSynthesis.onvoiceschanged = () => {
+  const voices = window.speechSynthesis.getVoices();
+  for (const voice of voices) {
+    const option = document.createElement("option");
+    option.textContent = `${voice.name} (${voice.lang})`;
+    option.setAttribute("data-lang", voice.lang);
+    option.setAttribute("data-name", voice.name);
+    voiceSelect.appendChild(option);
 
-//ATRIBUIÇÃO DE FUNÇÕES AO DISPLAY
-playBtn.addEventListener("click", iniciar);
-stopBtn.addEventListener("click", pausar);
-continuaBtn.addEventListener("click", continuar);
-cancelBtn.addEventListener("click", parar);
+    const playBtn = document.getElementById("playbtn");
+    const stopBtn = document.getElementById("stopbtn");
+    const continuaBtn = document.getElementById("resume");
+    const cancelBtn = document.getElementById("cancel");
+    const textArea = document.getElementById("input");
+    const msg = document.getElementById("msg");
 
-//FUNÇÕES DOS BOTOES DO DISPLAY
-const synthesizer = window.speechSynthesis;
+    //ATRIBUIÇÃO DE FUNÇÕES AO DISPLAY
+    playBtn.addEventListener("click", iniciar);
+    stopBtn.addEventListener("click", pausar);
+    continuaBtn.addEventListener("click", continuar);
+    cancelBtn.addEventListener("click", parar);
 
-//ESSA FUNÇÃO SERVE PARA PARAR O AUDIO QUANDO INICIAR A PAGINA, RESETANDO A FILA
-window.onload = function () {
-  synthesizer.cancel();
+    //FUNÇÕES DOS BOTOES DO DISPLAY
+
+    //ESSA FUNÇÃO SERVE PARA PARAR O AUDIO QUANDO INICIAR A PAGINA, RESETANDO A FILA
+    window.onload = function () {
+      synthesizer.cancel();
+    };
+
+    function parar() {
+      if (confirm("Deseja realmente parar a fala?")) {
+        synthesizer.cancel();
+      }
+      return;
+    }
+
+    const synthesizer = window.speechSynthesis;
+    function iniciar() {
+      //CONDIÇÃO PARA CASO A FALA ESTEJA EM ANDAMENTO
+      if (synthesizer.speaking) {
+        msg.textContent = "fala em andamento";
+        msg.classList.add("visivel");
+        //TEMPO DE VISIBILIDADE DA MENSAGEM
+        setTimeout(() => {
+          msg.classList.remove("visivel");
+        }, 1400);
+      } else {
+        const textContent = textArea.value;
+        let utterThis = new SpeechSynthesisUtterance(textContent);
+
+        utterThis.voice = voice;
+        synthesizer.speak(utterThis);
+      }
+    }
+
+    function pausar() {
+      const pausado = synthesizer.paused;
+      if (synthesizer.speak) {
+        pausado === false;
+      }
+      synthesizer.pause();
+    }
+
+    function continuar() {
+      synthesizer.resume();
+    }
+  }
 };
-
-function parar() {
-  if (confirm("Deseja realmente parar a fala?")) {
-    synthesizer.cancel();
-  }
-  return;
-}
-function iniciar() {
-  //CONDIÇÃO PARA CASO A FALA ESTEJA EM ANDAMENTO
-  if (synthesizer.speaking) {
-    msg.textContent = "fala em andamento";
-    msg.classList.add("visivel");
-    //TEMPO DE VISIBILIDADE DA MENSAGEM
-    setTimeout(() => {
-      msg.classList.remove("visivel");
-    }, 1400);
-  } else {
-    // synthesizer.cancel();
-    const textContent = textArea.value;
-    let voice = new SpeechSynthesisUtterance(textContent);
-    synthesizer.speak(voice);
-  }
-}
-
-function pausar() {
-  const pausado = synthesizer.paused;
-  if (synthesizer.speak) {
-    pausado === false;
-  }
-  synthesizer.pause();
-}
-
-function continuar() {
-  synthesizer.resume();
-}
-
-console.log(synthesizer);
